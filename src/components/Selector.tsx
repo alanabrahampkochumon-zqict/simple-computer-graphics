58e9b598fe83bc4ce11b5e1c775276d3ad8dca88
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-05 16:17:06 +0530

    feat: add header

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,8 +1,16 @@
+import { cn } from "../libs/utils";
+
 function Selector({ pages, selected }: { pages: string[]; selected: string }) {
     return (
         <ul className="inline-flex gap-0 p-1.5 self-end color-surface-secondary rounded-full">
             {pages.map((page) => (
-                <li className="color-content-tertiary ps-5 pe-5 pt-3 pb-3 rounded-full">
+                <li
+                    className={cn(
+                        "color-content-tertiary ps-5 pe-5 pt-3 pb-3 rounded-full cursor-pointer",
+                        selected === page &&
+                            "font-semibold color-content-accent color-surface-primary shadow-sm", // TODO:Change to a separate div to animate
+                    )}
+                >
                     {page}
                 </li>
             ))}
