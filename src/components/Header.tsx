a3c20de15487ce4a37ddc321bb39195cdd2b0b61
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-05 15:30:47 +0530

    feat: add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,10 @@ import Logo from "./Logo";
 
 function Header() {
     return (
-        <header>
-            <Logo />
+        <header className="color-surface-primary ps-20 pe-20 pt-6 pb-6">
+            <div>
+                <Logo />
+            </div>
         </header>
     );
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,13 @@
 function Logo() {
     return (
-        <div>
+        <div className="inline-grid gap-0.5">
             <p className="text-5xl font-bold">
-                <span>CS</span>
-                <span>4600</span>
+                <span className="color-content-accent">CS</span>
+                <span className="color-content-secondary">4600</span>
             </p>
-            <p>By Cem Yuskel</p>
+            <p className="text-sm font-light color-content-secondary text-end">
+                By Cem Yuskel
+            </p>
         </div>
     );
 }
