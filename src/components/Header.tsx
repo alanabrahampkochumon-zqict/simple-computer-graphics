190304573ce61ec446682d9e204d2bf2461cf561
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-05 15:31:53 +0530

    fix: add header shadow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 function Header() {
     return (
-        <header className="color-surface-primary ps-20 pe-20 pt-6 pb-6">
+        <header className="color-surface-primary ps-20 pe-20 pt-6 pb-6 shadow-xs">
             <div>
                 <Logo />
             </div>
