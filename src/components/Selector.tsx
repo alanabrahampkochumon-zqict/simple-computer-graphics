0d83439447f43de39d2a12e65b5c5e236e9e34ce
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-12 11:51:35 +0530

    feat: add animation to selector

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from "@tanstack/react-router";
 import { cn } from "../libs/utils";
+import styles from "./Selector.module.css";
 
 type SelectionParams = {
     pages: PageRoute[];
@@ -9,20 +10,30 @@ function Selector({ pages }: SelectionParams) {
     console.log(currentPath);
 
     return (
-        <ul className="inline-flex gap-0 p-1.5 self-end color-surface-secondary rounded-full">
+        <ul className="inline-flex gap-0 p-1.5 self-end color-surface-secondary rounded-full isolate">
             {pages.map((page) => (
-                <li
-                    className={cn(
-                        "color-content-tertiary ps-5 pe-5 pt-3 pb-3 rounded-full cursor-pointer",
-                        currentPath === page.link &&
-                            "font-semibold color-content-accent color-surface-primary shadow-sm", // TODO:Change to a separate div to animate
-                    )}
-                >
-                    <Link to={page.link} className="[&.active]:font-bold">
+                <li className="inline-flex items-center">
+                    <Link
+                        className={cn(
+                            "color-content-tertiary ps-5 pe-5 pt-3 pb-3 rounded-full cursor-pointer items-center",
+                            styles["selector-item"],
+                            currentPath === page.link &&
+                                "color-content-accent font-medium", //color-surface-primary
+                            currentPath === page.link &&
+                                styles["selector-selected"], // TODO:Change to a separate div to animate
+                        )}
+                        to={page.link}
+                    >
                         {page.title}
                     </Link>
                 </li>
             ))}
+            <div
+                className={cn(
+                    "color-surface-primary rounded-full shadow-sm",
+                    styles.anchor,
+                )}
+            />
         </ul>
     );
 }
