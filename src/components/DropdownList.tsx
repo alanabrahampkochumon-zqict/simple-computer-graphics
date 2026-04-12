f1af7ceb4ff58f796db3d0d367c109992915558f
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-12 15:10:47 +0530

    feat: add basic dropdown

---
+++ src/components/DropdownList.tsx
@@ -0,0 +1,55 @@
+import { DropdownMenu } from "radix-ui";
+// import { ReactComponent as ChevronDown } from "../assets/icon/chevron-down.svg"; // TODO: Fix
+import Label from "./Label";
+type DropdownListProps = {
+    label: string;
+    currentOption: string;
+    options: string[];
+    onOptionChange: (option: string) => void;
+};
+
+function DropdownList({
+    label,
+    currentOption,
+    options,
+    onOptionChange,
+}: DropdownListProps) {
+    return (
+        <div className="flex flex-col gap-2">
+            <Label>{label}</Label>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger asChild>
+                    <button
+                        className="color-surface-secondary rounded-2xl pl-4 pr-4 pt-3 pb-3 flex gap-2"
+                        aria-label="Customise options"
+                    >
+                        <span className="flex-1 text-left color-content-primary text-lg font-normal">
+                            {currentOption}
+                        </span>
+                        {/* <ChevronDown /> */}
+                    </button>
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Portal>
+                    <DropdownMenu.Content
+                        className="color-surface-primary w-[320px] shadow-xl p-8 pt-4 pb-4 rounded-2xl"
+                        sideOffset={5}
+                    >
+                        {options.map((option) => (
+                            <DropdownMenu.Item
+                                className="p-4 pl-2 pr-2 text-lg "
+                                onClick={() => onOptionChange(option)}
+                            >
+                                {option}
+                            </DropdownMenu.Item>
+                        ))}
+
+                        <DropdownMenu.Separator className="DropdownMenuSeparator" />
+                    </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+            </DropdownMenu.Root>
+        </div>
+    );
+}
+
+export default DropdownList;
--- src/routes/compositing/components/AdjustmentPanel.tsx
+++ src/routes/compositing/components/AdjustmentPanel.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import AdjustmentSlider from "../../../components/AdjustmentSlider";
+import DropdownList from "../../../components/DropdownList";
 
 function AdjustmentPanel() {
     const [opacity, setOpacity] = useState(100);
+    const blendingModes = ["Normal", "Multiply", "Add", "Subtract", "Divide"];
+    const [currentBlendMode, setCurrentBlendMode] = useState(blendingModes[0]);
     return (
         <aside className="pt-6 pb-4 pl-6 pr-4 color-surface-primary max-w-[400px] min-w-[320px] w-[35%] rounded-tl-3xl rounded-bl-3xl shadow-lg md:mt-4 md:mb-4">
             <h2 className="text-2xl font-semibold color-content-primary">
@@ -14,6 +17,12 @@ function AdjustmentPanel() {
                 onValueChange={(value) => {
                     setOpacity(value);
                 }}
+            />
+            <DropdownList
+                label="Blending Mode"
+                options={blendingModes}
+                currentOption={currentBlendMode}
+                onOptionChange={setCurrentBlendMode}
             />
         </aside>
     );
