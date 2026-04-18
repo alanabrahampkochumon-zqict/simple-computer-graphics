cc34b3618ba4dd5c3f0fc10a51ba4e02c9d2cc18
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-18 20:06:27 +0530

    fix: update dropdown spacing, hover state, and selected state'

--- src/components/DropdownList.tsx
+++ src/components/DropdownList.tsx
@@ -1,5 +1,4 @@
 import { DropdownMenu } from "radix-ui";
-// import { ReactComponent as ChevronDown } from "../assets/icon/chevron-down.svg"; // TODO: Fix
 import Label from "./Label";
 type DropdownListProps = {
     label: string;
@@ -7,6 +6,9 @@ type DropdownListProps = {
     options: string[];
     onOptionChange: (option: string) => void;
 };
+
+import { CheckIcon, ChevronDown } from "lucide-react";
+import { cn } from "../libs/utils";
 
 function DropdownList({
     label,
@@ -20,27 +22,38 @@ function DropdownList({
             <DropdownMenu.Root>
                 <DropdownMenu.Trigger asChild>
                     <button
-                        className="color-surface-secondary rounded-2xl pl-4 pr-4 pt-3 pb-3 flex gap-2"
-                        aria-label="Customise options"
+                        className="color-surface-secondary rounded-2xl pl-4 pr-4 pt-3 pb-3 flex gap-2 focus:outline-none hover:brightness-95 transition cursor-pointer"
+                        aria-label={label}
                     >
                         <span className="flex-1 text-left color-content-primary text-base font-normal">
                             {currentOption}
                         </span>
-                        {/* <ChevronDown /> */}
+                        <ChevronDown className="color-content-tertiary" />
                     </button>
                 </DropdownMenu.Trigger>
 
                 <DropdownMenu.Portal>
                     <DropdownMenu.Content
-                        className="color-surface-primary w-[320px] shadow-xl p-8 pt-4 pb-4 rounded-2xl"
+                        className="color-surface-primary w-[320px] shadow-xl p-4 pt-4 pb-4 rounded-2xl"
                         sideOffset={5}
                     >
                         {options.map((option) => (
                             <DropdownMenu.Item
-                                className="p-4 pl-2 pr-2 text-base"
+                                className={cn(
+                                    "p-4 pl-6 pr-6 color-surface-primary rounded-xl text-base cursor-pointer focus:outline-none hover:brightness-95 transition flex justify-between items-center",
+                                    option === currentOption &&
+                                        "color-content-accent",
+                                )}
                                 onClick={() => onOptionChange(option)}
                             >
                                 {option}
+                                {option === currentOption && (
+                                    <CheckIcon
+                                        className="color-content-accent"
+                                        width={20}
+                                        height={20}
+                                    />
+                                )}
                             </DropdownMenu.Item>
                         ))}
 
