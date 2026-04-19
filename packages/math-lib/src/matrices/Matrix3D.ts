1b5a643f494ed8125085a2ff075fa3d370ce457a
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-19 16:18:25 +0530

    feat: add basic mat3d, vec3d classes to math-lib

---
+++ packages/math-lib/src/matrices/Matrix3D.ts
@@ -0,0 +1,7 @@
+export class Matrix3D {
+    public readonly buffer: Float32Array;
+
+    constructor() {
+        this.buffer = new Float32Array(9);
+    }
+}
---
+++ packages/math-lib/src/vectors/Vector3D.ts
@@ -0,0 +1,7 @@
+export class Vector3D {
+    public readonly buffer: Float32Array;
+
+    constructor() {
+        this.buffer = new Float32Array(3);
+    }
+}
