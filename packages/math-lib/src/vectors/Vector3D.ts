20cfae6b2de46803256557f910ad24915e89b4f9
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-09 16:49:14 +0530

    feat: add Vector3D constructor + x, y, z accessors

--- packages/math-lib/src/vectors/Vector3D.ts
+++ packages/math-lib/src/vectors/Vector3D.ts
@@ -1,7 +1,28 @@
 export class Vector3D {
     public readonly buffer: Float32Array;
 
-    constructor() {
+    /**
+     * Instantiate a 3D vector with its components.
+     * @param x The x component.
+     * @param y The y component.
+     * @param z The z component.
+     */
+    constructor(x: number, y: number, z: number) {
         this.buffer = new Float32Array(3);
+        this.buffer[0] = x;
+        this.buffer[1] = y;
+        this.buffer[2] = z;
+    }
+
+    x() {
+        return this.buffer[0];
+    }
+
+    y() {
+        return this.buffer[1];
+    }
+
+    z() {
+        this.buffer[2];
     }
 }
