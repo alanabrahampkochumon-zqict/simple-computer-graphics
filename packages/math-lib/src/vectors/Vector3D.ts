a0fc39cca1443f09c9c57a6f6993d8834a16998d
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-09 16:54:30 +0530

    feat: add Vector3D setters

--- packages/math-lib/src/vectors/Vector3D.ts
+++ packages/math-lib/src/vectors/Vector3D.ts
@@ -3,9 +3,9 @@ export class Vector3D {
 
     /**
      * Instantiate a 3D vector with its components.
-     * @param x The x component.
-     * @param y The y component.
-     * @param z The z component.
+     * @param x The x-component.
+     * @param y The y-component.
+     * @param z The z-component.
      */
     constructor(x: number, y: number, z: number) {
         this.buffer = new Float32Array(3);
@@ -14,15 +14,68 @@ export class Vector3D {
         this.buffer[2] = z;
     }
 
+    /**
+     * Return the x-component of this vector.
+     *
+     * @returns The x-component of the vector.
+     */
     x() {
         return this.buffer[0];
     }
 
+
+    /**
+     * Return the y-component of this vector.
+     *
+     * @returns The y-component of the vector.
+     */
     y() {
         return this.buffer[1];
     }
 
+
+    /**
+     * Return the z-component of this vector.
+     *
+     * @returns The z-component of the vector.
+     */
     z() {
-        this.buffer[2];
+        return this.buffer[2];
+    }
+
+
+    /**
+     * Set the x-component of this vector the provided value.
+     * @param value The value to set as x-component.
+     *
+     * @returns The current instance to enable chaining.
+     */
+    setX(value: number): Vector3D {
+        this.buffer[0] = value;
+        return this;
+    }
+
+
+    /**
+     * Set the y-component of this vector the provided value.
+     * @param value The value to set as y-component.
+     *
+     * @returns The current instance to enable chaining.
+     */
+    setY(value: number): Vector3D {
+        this.buffer[1] = value;
+        return this;
+    }
+
+
+    /**
+     * Set the z-component of this vector the provided value.
+     * @param value The value to set as z-component.
+     *
+     * @returns The current instance to enable chaining.
+     */
+    setZ(value: number): Vector3D {
+        this.buffer[2] = value;
+        return this;
     }
 }
